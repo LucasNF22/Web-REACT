87bcb4d1974ed265d6ccb5ea70335e677047adf4
LucasNF22 <lucasnfiorentino@gmail.com>
2023-08-25 21:00:48 -0300

    Inicio de la seccion Work

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,51 +1,88 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Scrollbar } from "swiper";
+import { BsArrowRight } from "react-icons/bs";
+import Image from "next/image";
+
+import "swiper/css";
+import "swiper/css/free-mode";
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
 // data
-const workSlides = {
-  slides: [
-    {
-      images: [
+export const workSlider = {
+    slides: [
         {
-          title: 'title',
-          path: '/thumb1.jpg',
+            images: [
+                {
+                    title: "title",
+                    path: "/thumb1.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb2.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb3.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb4.jpg",
+                },
+            ],
         },
         {
-          title: 'title',
-          path: '/thumb2.jpg',
+            images: [
+                {
+                    title: "title",
+                    path: "/thumb4.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb1.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb2.jpg",
+                },
+                {
+                    title: "title",
+                    path: "/thumb3.jpg",
+                },
+            ],
         },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-      ],
-    },
-    {
-      images: [
-        {
-          title: 'title',
-          path: '/thumb4.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb1.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb2.jpg',
-        },
-        {
-          title: 'title',
-          path: '/thumb3.jpg',
-        },
-      ],
-    },
-  ],
+    ],
 };
 
 const WorkSlider = () => {
-  return <div>Work Slider</div>;
+    return (
+        <Swiper
+       
+            spaceBetween={10}
+            pagination={{
+              clickable: true
+            }}
+            scrollbar={{ draggable: true }}
+            modules={[Pagination, Scrollbar]}
+            className="h-[280px] sm:h-[480px]"
+        >
+            {workSlider.slides.map((slide, index) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                      {slide.images.map((image, index) => {
+                        return (
+                        <div>
+                          <div>
+                            <Image key={index} src={image.path} width={500} height={300} alt='' />
+                          </div>
+                        </div>)
+                      })}
+                    </div>
+                  </SwiperSlide>)
+            })}
+        </Swiper>
+    );
 };
 
 export default WorkSlider;
--- pages/work/index.js
+++ pages/work/index.js
@@ -1,9 +1,53 @@
+import WorkSlider from '../../components/WorkSlider'
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 
 const Work = () => {
   return (
-    <div className="h-full bg-">
-      <div>
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      
+      <div className="container mx-auto">
+        
+        <div className="flex flex-col xl:flex-row gap-x-8 ">
+
+          <div className='text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0'>
+            <motion.h2
+              className='h2 xl:mt-8'
+              variants={fadeIn('up', 0.3)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+
+            >
+              Hola <span className='text-accent'>Mundo</span>
+            </motion.h2>
+            <motion.p
+              className='mb-4 max-w-[400px] mx-auto lg:mx-0'
+              variants={fadeIn('up', 0.4)}
+              initial='hidden'
+              animate='show'
+              exit='hidden'
+            >
+              A alalala lala longg...  dwskhflasdbfasdliflaskdhf
+              lakjsdhflkajshflkjashdflkjahsfsdfasdfgasdf
+              asdfasdfasdfasdfasfasdf.asdfasdfasdfasdfasfasdfasdfasdf
+            </motion.p>
+
+          </div>
+          
+          <motion.div 
+            className='w-full xl:max-w-[65%]'
+            variants={fadeIn('down', 0.6)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+          >
+            <WorkSlider />
+          </motion.div>
+
+        </div>
 
       </div>
     </div>
