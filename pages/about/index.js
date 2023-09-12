3f56463aec99c4c03de91b27b570d339cc3dcee4
LucasNF22 <lucasnfiorentino@gmail.com>
2023-09-12 13:20:07 -0300

    Modificando seccion about

--- pages/about/index.js
+++ pages/about/index.js
@@ -9,38 +9,45 @@ const About = () => {
 
 
   return (
-    <div className="h-full py-16 xl:py-32 text-center xl:text-left ">
+    // <div className="h-full py-16 xl:py-32 text-center xl:text-left ">
       
-      <div className="mx-auto flex flex-col gap-y-8 lg:items-start xl:flex-row gap-x-20 pl-10">
+    //   <div className="mx-auto flex flex-col gap-y-8 lg:items-start xl:flex-row gap-x-20 pl-10">
         
-        <div className="w-full flex flex-row gap-10">
-          <div>
-            <h2 className="h2 text-center">
-              <span className="text-secondary">{"{"}</span>
-              Hola
-              <span className="text-accent">Mundo</span>
-              <span className="text-secondary">{"}"}</span>
-            </h2>
-              <p >dkjfbksdjfh djfhl jfhlasjfh</p>
-          </div>
-            <div>
-              <h1 className="text-2xl pb-4">un poco sobre mi</h1>
-              <div className="">               
-                  <p className="">{"Desarrollo web"}<br/> </p>
-                  <p className="">{"Diseño"}<br/> </p>
-                  <p className="">{"Tecnología"}<br/> </p>
-                  <p className="">{"Gaming"}<br/> </p>
-                  <p className="">{"Fernet"}<br/> </p>                 
-              </div>      
-            </div>
+    //     <div className="w-full flex flex-row gap-10">
+    //       <div>
+    //         <h2 className="h2 text-center">
+    //           <span className="text-secondary">{"{"}</span>
+    //           Hola
+    //           <span className="text-accent">Mundo</span>
+    //           <span className="text-secondary">{"}"}</span>
+    //         </h2>
+    //           <p >dkjfbksdjfh djfhl jfhlasjfh</p>
+    //       </div>
+    //         <div>
+    //           <h1 className="text-2xl pb-4">un poco sobre mi</h1>
+    //           <div className="">               
+    //               <p className="">{"Desarrollo web"}<br/> </p>
+    //               <p className="">{"Diseño"}<br/> </p>
+    //               <p className="">{"Tecnología"}<br/> </p>
+    //               <p className="">{"Gaming"}<br/> </p>
+    //               <p className="">{"Fernet"}<br/> </p>                 
+    //           </div>      
+    //         </div>
           
-        </div>
+    //     </div>
       
       
      
       
-      </div>
-    </div>
+    //   </div>
+    // </div>
+
+    <h2 className="h2 text-center">
+       <span className="text-secondary">{"{"}</span>
+       En 
+       <span className="text-accent">Construcción</span>
+       <span className="text-secondary">{"}"}</span>
+    </h2>
   );
 };
 
